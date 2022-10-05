55ada17d03ee8e76edf06e4fa1a1de81ed03e7e6
liubov <zamula.l08l@gmail.com>
2022-10-05 19:08:53 +0900

    radio button gives value to output now

--- app/src/main/java/com/example/shinkansen/MainActivity.kt
+++ app/src/main/java/com/example/shinkansen/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.shinkansen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.*
-import com.google.android.material.textfield.TextInputLayout
 import org.w3c.dom.Text
 import java.util.*
 
@@ -13,17 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
-
-        rollButton.setOnClickListener {
-            rollDice()
-        }
-
         val buttonOK: Button = findViewById(R.id.buttonOK)
         buttonOK.setOnClickListener {
             showTrainDetails()
         }
-
     }
 
     private fun showTrainDetails() {
@@ -34,21 +25,12 @@ class MainActivity : AppCompatActivity() {
         val fromText = fromTextField.text
         val toText = toTextField.text
 
-        trainDetails.text = "Train from $fromText to $toText at 15:00"
-    }
-    private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
-        val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_3
-        }
-        val diceImage: ImageView = findViewById(R.id.dice_image)
-        diceImage.setImageResource(drawableResource)
+        val radioGroup: RadioGroup = findViewById(R.id.radioGroup)
+        val intSelectButton: Int = radioGroup!!.checkedRadioButtonId
+        val radioButton: TextView = findViewById(intSelectButton)
+        //Toast.makeText(baseContext, radioButton.text, Toast.LENGTH_SHORT).show()
+        val time = radioButton.text
+
+        trainDetails.text = "Train from $fromText to $toText at $time"
     }
 }
